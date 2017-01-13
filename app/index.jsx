2ea062beb3f12b0ecfabe8a92724ce1b8c0e4e83
Fabio Mazzone <fabio.mazzone@me.com>
2017-01-13 18:32:01 +0100

    added hello world

--- app/index.jsx
+++ app/index.jsx
@@ -1,2 +1,6 @@
-import react from 'react';
-import reactDom from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
+
+const appMount = document.getElementById('app');
+
+render(<h1>Hello World</h1>, appMount);
