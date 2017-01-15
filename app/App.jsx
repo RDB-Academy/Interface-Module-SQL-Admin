b882288ed6dd7dc4f10c459ac1f7169949fdbf70
Fabio Mazzone <fabio.mazzone@me.com>
2017-01-15 13:54:33 +0100

    asjdkl

--- app/App.jsx
+++ app/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { Provider }  from 'react-redux';
+import { Provider } from 'react-redux';
+
+import Router from 'react-router/BrowserRouter'
+import Match from 'react-router/Match'
+import Link from 'react-router/Link'
 
 const App = ({ store }) => (
   <Provider store={store}>
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    </Router>
   </Provider>
 )
 
