b171beffa12808e3247803d98452b295cc3489a7
Fabio Mazzone <fabio.mazzone@me.com>
2017-04-06 16:20:17 +0200

    removed index.prod.jsx #25

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,34 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./index.prod.jsx'); // eslint-disable-line
-} else {
-  module.exports = require('./index.dev.jsx'); // eslint-disable-line
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import { AppContainer } from 'react-hot-loader'; // eslint-disable-line
+
+import App from 'App';
+import configureStore from './store';
+
+const appMount = document.getElementById('root');
+
+const history = createBrowserHistory({ basename: '/admin' });
+const store = configureStore(history);
+
+ReactDOM.render(
+  <AppContainer>
+    <Provider store={store}>
+      <App history={history} />
+    </Provider>
+  </AppContainer>,
+  appMount);
+
+if (module.hot) {
+  module.hot.accept('App', () => {
+    const NewApp = require('App').default; // eslint-disable-line
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <NewApp history={history} />
+        </Provider>
+      </AppContainer>,
+      appMount);
+  });
 }
